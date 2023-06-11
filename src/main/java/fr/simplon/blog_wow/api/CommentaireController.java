0a0198ab8cfa8e6f03e8c573afeaaf8ac2dcdf27
massoue <121940193+Massouej@users.noreply.github.com>
2023-06-11 15:52:16 +0200

    Correction bug sur le message d'erreur lors d'une tentative de modif OK

--- src/main/java/fr/simplon/blog_wow/api/CommentaireController.java
+++ src/main/java/fr/simplon/blog_wow/api/CommentaireController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -18,10 +17,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 import java.security.Principal;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 /**
